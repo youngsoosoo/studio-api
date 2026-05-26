e963c035223af23e38c48a8ad2492199264683ab
youngsoosoo <lovepys01@naver.com>
2026-05-26 20:46:49 +0900

    chore: initialize spring boot gradle project

---
+++ src/main/java/com/studio/api/StudioApiApplication.java
@@ -0,0 +1,13 @@
+package com.studio.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudioApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/studio/api/StudioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studio.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
